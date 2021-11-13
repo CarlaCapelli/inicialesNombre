21fcf78c2853a39afdab6ba7eee1b14913da9c37
CarlaCapelli <92063946+CarlaCapelli@users.noreply.github.com>
2021-11-13 15:51:44 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+let nombreApellido: string = "Carla Capelli";
+let inicialNombre: string = nombreApellido[0];
+let inicialApellido: string = nombreApellido[6];
+console.log(
+  "nombre y apellido",
+  nombreApellido,
+  "iniciales",
+  inicialNombre,
+  inicialApellido
+);
